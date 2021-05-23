0d8c556a28010b05e7184f65b87c6d68263b2945
blindfolded32 <sunsya@yandex.ru>
2021-05-23 22:21:08 +0300

    bomb logic

--- Assets/Scripts/BombExplosion.cs
+++ Assets/Scripts/BombExplosion.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class BombExplosion : MonoBehaviour
 {
-    [SerializeField] WallDestroy boom;
+   // [SerializeField] WallDestroy boom;
+    private List<GameObject> Triggered;
 
     public void OnTriggerEnter(Collider other)
     {
         print("Boom");
-        boom.DestroyIt();
+     //   boom.DestroyIt();
+        gameObject.GetComponent<SphereCollider>().isTrigger=true;
+        Triggered.Add(other.gameObject);
+        foreach (GameObject Bommbed in Triggered) Bommbed.GetComponent<Rigidbody>().AddForce(new Vector3(Bommbed.transform.position.x * -1, 50, Bommbed.transform.position.z * -1), ForceMode.Impulse);
+
         Destroy(gameObject);
+
     }
 }
